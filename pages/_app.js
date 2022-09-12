bd60e3ef2380dd38cffb80c8867cefbf65553c13
Rupam Shil <rupamshil111@gmail.com>
2022-09-12 20:41:10 +0530

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.scss';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>Axon</title>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
