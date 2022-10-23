2e09c44f102a7656f2e2ab11b68a4726e2a9c44d
Rupam Shil <rupamshil111@gmail.com>
2022-10-23 21:14:31 +0530

    xx

--- features/Hero/components/MobileMockup.js
+++ features/Hero/components/MobileMockup.js
@@ -1,7 +1,34 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import Styles from '../../../styles/scoped/Mobile.module.scss';
+import MusicCard from './MusicCard';
 function MobileMockup() {
+	const musicCard = [
+		{
+			songName: 'In The Garden',
+			type: ['Theta', 'Relax', 'Calm'],
+			fill: '#148AC5',
+			songLink: '/music/in-the-garden.mp3',
+		},
+		{
+			songName: 'Oh Freedom',
+			type: ['Theta', 'Resting', 'Somatic'],
+			fill: '#B77D8E',
+			songLink: '/music/oh-freedom.mp3',
+		},
+		{
+			songName: 'A Mighty Fortress',
+			type: ['Theta', 'Anxious', 'Trauma &'],
+			fill: '#8C6CA0',
+			songLink: '/music/a-mighty-fortress.mp3',
+		},
+		{
+			songName: 'Jesus Calls Us',
+			type: ['Delta', 'Focus', 'Calm'],
+			fill: '#5F9EB8',
+			songLink: '/music/jesus-calls-us.mp3',
+		},
+	];
 	return (
 		<div className={Styles.mobile}>
 			<img
@@ -9,7 +36,17 @@ function MobileMockup() {
 				alt="mobilemockup"
 				className={Styles.img}
 			/>
-			<div className={Styles.con}></div>
+			<div className={Styles.con}>
+				{musicCard.map((card, i) => (
+					<MusicCard
+						key={i}
+						songName={card.songName}
+						fill={card.fill}
+						types={card.type}
+						songLink={card.songLink}
+					/>
+				))}
+			</div>
 		</div>
 	);
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,45 @@
 import '../styles/globals.scss';
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
+	const [render, setRender] = useState(true);
+	useEffect(() => {
+		handleResize();
+		window.addEventListener('resize', handleResize);
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+	let handleResize = () => {
+		if (window.innerWidth <= 920) {
+			setRender(false);
+			return;
+		}
+		setRender(true);
+	};
 	return (
 		<>
 			<Head>
 				<title>Axon</title>
 			</Head>
-			<Component {...pageProps} />
+			{render ? (
+				<Component {...pageProps} />
+			) : (
+				<div
+					style={{
+						fontSize: '2rem',
+						display: 'flex',
+						width: '100vw',
+						height: '100vh',
+						justifyContent: 'center',
+						alignItems: 'center',
+						textAlign: 'center',
+					}}
+				>
+					This site is not mobile responsive at the moment!👻
+				</div>
+			)}
 		</>
 	);
 }
